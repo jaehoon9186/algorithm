74badd98f4c0c58e7e7f1b27d8fcbad040a8a9d4
jaehoon9186 <hunhun9186@gmail.com>
2021-08-25 12:29:51 +0900

    연결 리스트

--- pythonProject1/13_palindrome_linked_list.py
+++ pythonProject1/13_palindrome_linked_list.py
@@ -21,9 +21,13 @@ The number of nodes in the list is in the range [1, 10**5].
 
 Follow up: Could you do it in O(n) time and O(1) space?
 """
+# 내풀이
+# pass
+
+# 풀이 1
+from typing import List, Optional
 
 
-# 내 풀이
 # Definition for singly-linked list.
 class ListNode:
     def __init__(self, val=0, next=None):
@@ -31,12 +35,10 @@ class ListNode:
         self.next = next
 
 
-from typing import Optional
-
-
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        q: list = []
+        q: List = []
+
         if not head:
             return True
 
@@ -46,6 +48,7 @@ class Solution:
             q.append(node.val)
             node = node.next
 
+
         # 팰린드롬 판별
         while len(q) > 1:
             if q.pop(0) != q.pop():
@@ -53,8 +56,62 @@ class Solution:
 
         return True
 
+# 풀이 2 #input이 좀 다름
+# import collections
+# from typing import Deque
+#
+#
+# # Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+#
+#
+# class Solution:
+#     def isPalindrome(self, head: ListNode) -> bool:
+#         # 데크 자료형 선언
+#         q: Deque = collections.deque()
+#
+#         if not head:
+#             return True
+#
+#         node = head
+#         while node is not None:
+#             q.append(node.val)
+#             node = node.next
+#
+#         while len(q) > 1:
+#             if q.popleft() != q.pop():
+#                 return False
+#
+#         return True
+
+# 풀이 4
+# # Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+#
+#
+# class Solution:
+#     def isPalindrome(self, head: ListNode) -> bool:
+#         rev = None
+#         slow = fast = head
+#         # 런너를 이용해 역순 연결 리스트 구성
+#         while fast and fast.next:
+#             fast = fast.next.next
+#             rev, rev.next, slow = slow, rev, slow.next
+#         if fast:
+#             slow = slow.next
+#
+#         # 팰린드롬 여부 확인
+#         while rev and rev.val == slow.val:
+#             slow, rev = slow.next, rev.next
+#         return not rev
 
 if __name__=="__main__":
     sol = Solution()
-    head = ListNode([1, 2, 2, 2])
+    head = ListNode(1,ListNode(2, ListNode(2, ListNode(1, None))))
     print(sol.isPalindrome(head))
