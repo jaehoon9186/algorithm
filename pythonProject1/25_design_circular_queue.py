e44f1f8a803e37e413f128d5023e2216ed33eb30
jaehoon9186 <hunhun9186@gmail.com>
2021-08-31 11:43:51 +0900

    스택, 큐

---
+++ pythonProject1/25_design_circular_queue.py
@@ -0,0 +1,151 @@
+"""
+25. 원형 큐 디자인
+622. design circular queue
+https://leetcode.com/problems/design-circular-queue/
+
+Design your implementation of the circular queue.
+The circular queue is a linear data structure in which the operations are performed based on FIFO (First In First Out) principle and the last position is connected back to the first position to make a circle. It is also called "Ring Buffer".
+
+One of the benefits of the circular queue is that we can make use of the spaces in front of the queue. In a normal queue, once the queue becomes full, we cannot insert the next element even if there is a space in front of the queue. But using the circular queue, we can use the space to store new values.
+
+Implementation the MyCircularQueue class:
+* MyCircularQueue(k) Initializes the object with the size of the queue to be k.
+* int Front() Gets the front item from the queue. If the queue is empty, return -1.
+* int Rear() Gets the last item from the queue. If the queue is empty, return -1.
+* boolean enQueue(int value) Inserts an element into the circular queue. Return true if the operation is successful.
+* boolean deQueue() Deletes an element from the circular queue. Return true if the operation is successful.
+* boolean isEmpty() Checks whether the circular queue is empty or not.
+* boolean isFull() Checks whether the circular queue is full or not.
+
+You must solve the problem without using the built-in queue data structure in your programming language.
+
+
+
+Example 1:
+Input
+["MyCircularQueue", "enQueue", "enQueue", "enQueue", "enQueue", "Rear", "isFull", "deQueue", "enQueue", "Rear"]
+[[3], [1], [2], [3], [4], [], [], [], [4], []]
+Output
+[null, true, true, true, false, 3, true, true, true, 4]
+
+Explanation
+MyCircularQueue myCircularQueue = new MyCircularQueue(3);
+myCircularQueue.enQueue(1); // return True
+myCircularQueue.enQueue(2); // return True
+myCircularQueue.enQueue(3); // return True
+myCircularQueue.enQueue(4); // return False
+myCircularQueue.Rear();     // return 3
+myCircularQueue.isFull();   // return True
+myCircularQueue.deQueue();  // return True
+myCircularQueue.enQueue(4); // return True
+myCircularQueue.Rear();     // return 4
+
+
+Constraints:
+* 1 <= k <= 1000
+* 0 <= value <= 1000
+* At most 3000 calls will be made to enQueue, deQueue, Front, Rear, isEmpty, and isFull.
+"""
+
+
+# 내 풀이
+class MyCircularQueue:
+
+    def __init__(self, k: int):
+        self.q = [None] * k
+        self.max_size = k
+        self.front = 0
+        self.rear = 0
+
+    def enQueue(self, value: int) -> bool:
+        if self.q[self.rear] is None:
+            self.q[self.rear] = value
+            self.rear += 1
+            if self.rear == self.max_size:
+                self.rear = 0
+            return True
+        else:
+            return False
+
+    def deQueue(self) -> bool:
+        if self.q[self.front] is not None:
+            self.q[self.front] = None
+            self.front += 1
+            if self.front == self.max_size:
+                self.front = 0
+            return True
+        else:
+            return False
+
+    def Front(self) -> int:
+        if self.isEmpty():
+            return -1
+        else:
+            return self.q[self.front]
+
+    def Rear(self) -> int:
+        if self.isEmpty():
+            return -1
+        else:
+            if self.rear - 1 < 0:
+                return self.q[-1]
+            else:
+                return self.q[self.rear - 1]
+
+    def isEmpty(self) -> bool:
+        return (self.rear - self.front == 0) and (self.q[self.front] is None)
+
+    def isFull(self) -> bool:
+        return (self.rear - self.front == 0) and (self.q[self.front] is not None)
+
+
+# Your MyCircularQueue object will be instantiated and called as such:
+# obj = MyCircularQueue(k)
+# param_1 = obj.enQueue(value)
+# param_2 = obj.deQueue()
+# param_3 = obj.Front()
+# param_4 = obj.Rear()
+# param_5 = obj.isEmpty()
+# param_6 = obj.isFull()
+
+# 풀이 1
+# class MyCircularQueue:
+#     def __init__(self, k: int):
+#         self.q = [None] * k
+#         self.maxlen = k
+#         self.p1 = 0
+#         self.p2 = 0
+#
+#     # enQueue(): 리어 포인터 이동
+#     def enQueue(self, value: int) -> bool:
+#         if self.q[self.p2] is None:
+#             self.q[self.p2] = value
+#             self.p2 = (self.p2 + 1) % self.maxlen
+#             return True
+#         else:
+#             return False
+#
+#     # deQueue(): 프론트 포인터 이동
+#     def deQueue(self) -> bool:
+#         if self.q[self.p1] is None:
+#             return False
+#         else:
+#             self.q[self.p1] = None
+#             self.p1 = (self.p1 + 1) % self.maxlen
+#             return True
+#
+#     def Front(self) -> int:
+#         return -1 if self.q[self.p1] is None else self.q[self.p1]
+#
+#     def Rear(self) -> int:
+#         return -1 if self.q[self.p2 - 1] is None else self.q[self.p2 - 1]
+#
+#     def isEmpty(self) -> bool:
+#         return self.p1 == self.p2 and self.q[self.p1] is None
+#
+#     def isFull(self) -> bool:
+#         return self.p1 == self.p2 and self.q[self.p1] is not None
+
+if __name__ == "__main__":
+    circular = MyCircularQueue(5)
+    print(circular.isEmpty())
